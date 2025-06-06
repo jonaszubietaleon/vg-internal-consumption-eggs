bc7af274d77bf18508b7dc17787942baaf7430c8
jonas Zubieta león <89934911+jonaszubietaleon@users.noreply.github.com>
2025-06-06 09:26:33 +0000

    nuevos cambios de confi

--- src/main/java/pe/edu/vallegrande/vg_ms_casas/config/SecurityConfig.java
+++ src/main/java/pe/edu/vallegrande/vg_ms_casas/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOriginPatterns(List.of("*")); 
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
